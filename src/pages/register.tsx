a7690f02cb99efebb82fa4d75d42bdecc5454367
Steve LeValley <slevalley@gmail.com>
2022-08-29 21:00:44 -0700

    Stopped at 2:46:00 and Added urql and hooked up register mutation

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -10,15 +10,38 @@ import {
 } from "@chakra-ui/react";
 import Wrapper from "../components/Wrapper";
 import { InputField } from "../components/inputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!) {
+  register(options: {username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      createdAt
+      updatedAt
+      username
+    }
+  }
+}
+`;
+
 export const Register: React.FC<registerProps> = ({}) => {
+  const [{}, register] = useMutation(REGISTER_MUT);
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={async (values) => {
+          //const response = await register({ username: values.username, password: values.password });
+          const response = await register(values);
+        }}
       >
         {({ values, handleChange, isSubmitting }) => (
           <Form>
@@ -36,6 +59,7 @@ export const Register: React.FC<registerProps> = ({}) => {
               />
             </Box>
             <Button
+              mt={4}
               type="submit"
               //   color=''
               isLoading={isSubmitting}
